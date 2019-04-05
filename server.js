cd02de7f2b957a5867b12bec7e6bcd8f36ade8ba
greison <greisonsantos03@gmail.com>
2019-04-05 16:27:34 -0300

    models, routes  e controllers

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+const requireDir =require('require-dir');
 
+//inicia app
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send('hello word !!');
-});
+//connect DB
+// mongoose.connect('mongo://user@password')
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser: true});
 
+
+requireDir('./src/models');
+
+
+
+//first route
+//use aceita todo tipo de requisição
+
+app.use('/api', require('./src/routes'));
 
 app.listen(3000);
 
