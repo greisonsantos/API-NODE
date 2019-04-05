fc6f15eb4b8f5096e3532fc1ed5ba7051fafd4c9
greison <greisonsantos03@gmail.com>
2019-04-05 14:57:37 -0300

    configuração nodemon

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('hello word !!');
+});
+
+
+app.listen(3000);
+
+
+//npm run dev
+// executa script do package.json
